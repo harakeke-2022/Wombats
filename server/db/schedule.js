0c47f23af0dfe3e5436a89842c20367ce5448fe3
Qian Han <kategreat1@hotmail.com>
2022-02-11 11:14:58 +1300

    done route & database

--- server/db/schedule.js
+++
@@ -1,9 +0,0 @@
-const connection = require('./connection')
-
-function getSchedule (db = connection) {
-  return db('schedule').select()
-}
-
-module.exports = {
-  getSchedule
-}
--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -1,18 +1,32 @@
 const express = require('express')
-
-const db = require('../db/schedule')
-
 const router = express.Router()
+
+const db = require('../db/db')
 
 router.get('/', (req, res) => {
   db.getSchedule()
-    .then(results => {
-      res.json({ schedule: results.map(schedule => schedule.name) })
+    .then(schedule => {
+      setTimeout(() => res.json(schedule)
+        , 1000)
       return null
     })
     .catch(err => {
-      console.log(err)
-      res.status(500).json({ message: 'Somthing went wrong' })
+      console.error(err)
+      res.status(500).json({ message: 'error in server' })
+    })
+})
+
+router.post('/', (req, res) => {
+  const { name, song, url } = req.body
+
+  db.addSchedule({ name, song, url })
+    .then(() => { // ignore ids from db function
+      res.sendStatus(201)
+      return null
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ message: 'error in server' })
     })
 })
 
