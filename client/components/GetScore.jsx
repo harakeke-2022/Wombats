7550e30f72c9fbc7b2c2df1f15f2c11187d906d9
Sadie <Sadie@Sadies-Macbook.local>
2022-02-11 15:48:59 +1300

    CSS Complete

--- client/components/GetScore.jsx
+++ client/components/GetScore.jsx
@@ -4,15 +4,15 @@ function GetScore () {
   const [score, setScore] = useState('')
 
   function handleClick () {
-    setScore (`Your score is ${(Math.floor(Math.random() * (100 - 50)) + 50)}. Well done, you legend!`)
+    setScore(`Your score is ${(Math.floor(Math.random() * (100 - 50)) + 50)}. Well done, you legend!`)
   }
 
-    return (
+  return (
     <>
-      <div>
-       <button className="discoBtn" onClick={console.log(`This does nothing!`)}>Listening...</button>
-       <button className="discoBtn" id="randomnumber" onClick={handleClick}>Score!</button>
-       <p>{score}</p>
+      <div className="flex-container2">
+        <button className="discoBtn" onClick={console.log('This does nothing!')}>Listening...</button>
+        <button className="discoBtn" id="randomnumber" onClick={handleClick} >Score!</button>
+        <p ID="scoreText">{score}</p>
       </div>
     </>
   )
